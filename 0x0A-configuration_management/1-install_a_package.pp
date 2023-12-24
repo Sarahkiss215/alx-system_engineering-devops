273db003cac278f4e09d1f3a50d527b037cc7c22
Sarahkiss215 <skisiangani5@gmail.com>
2023-12-24 10:27:38 -0800

    Update the file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install puppet-lint 2.5.0
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Installs puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
