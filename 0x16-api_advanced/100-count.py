a2da7689ec2808afeed57afb62a162279ce08517
Sarahkiss215 <skisiangani5@gmail.com>
2024-02-13 04:17:03 -0800

    Add queries the Reddit API, parses the title of all hot articles, and prints

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Queries the Reddit API, parses title hot articles, and prints it"""
+
+
+from requests import get
+
+REDDIT = "https://www.reddit.com/"
+HEADERS = {'user-agent': 'my-app/0.0.1'}
+
+
+def count_words(subreddit, word_list, after="", word_dic={}):
+    """Returns a list containing the titles of all hot articles"""
+    if not word_dic:
+        for word in word_list:
+            word_dic[word] = 0
+
+    if after is None:
+        word_list = [[key, value] for key, value in word_dic.items()]
+        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
+        for w in word_list:
+            if w[1]:
+                print("{}: {}".format(w[0].lower(), w[1]))
+        return None
+
+    url = REDDIT + "r/{}/hot/.json".format(subreddit)
+
+    params = {
+        'limit': 100,
+        'after': after
+    }
+
+    r = get(url, headers=HEADERS, params=params, allow_redirects=False)
+
+    if r.status_code != 200:
+        return None
+
+    try:
+        jscript = r.json()
+
+    except ValueError:
+        return None
+
+    try:
+
+        data = jscript.get("data")
+        after = data.get("after")
+        children = data.get("children")
+        for child in children:
+            post = child.get("data")
+            title = post.get("title")
+            lower = [s.lower() for s in title.split(' ')]
+
+            for w in word_list:
+                word_dic[w] += lower.count(w.lower())
+
+    except:
+        return None
+
+    count_words(subreddit, word_list, after, word_dic)
